9690a0dc02280f45d8425003800c95b23a48fe55
Anatolii K <tol890@ukr.net>
2020-09-05 20:01:32 +0300

    fetch

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get Quote from API
+async function getQuote() {
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.log('whoops, no quote', error);
+    }
+}
+
+// On Load
+getQuote();
