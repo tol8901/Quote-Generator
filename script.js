993330562fc84816b7b905832f13c17189ff8b15
Anatolii K <tol890@ukr.net>
2020-09-06 16:00:05 +0300

    the project is finished

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// Show loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading 
-function complete() {
+function removeLoadingSpinner() {
     if (!loader.hidden) {
         quoteContainer.hidden = false;
         loader.hidden = true;
@@ -21,7 +19,7 @@ function complete() {
 
 // Get Quote from API
 async function getQuote() {
-    loading();
+    showLoadingSpinner();
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -41,7 +39,7 @@ async function getQuote() {
         }
         quoteText.innerText = data.quoteText;
         // Stop Loader, Show the quote
-        complete();
+        removeLoadingSpinner();
     } catch (error) {
         getQuote();
     }
